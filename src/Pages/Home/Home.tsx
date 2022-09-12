b01628dac022bd6c91e4351f7fdc617cc57107f9
pdrlcx <mendesv460@gmail.com>
2022-09-12 19:42:24 -0300

    Adding New Files to Personal Blog

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import'./Home.css'
+
+function Home(){
+    return ();
+    <div>
+        <h1>Home</h1>
+        <p><img src="C:\Users\Positivo\Desktop\Repositorios\Blog_Pessoal_React\blog-pessoal\src\Assets\Notre_Dame_Logo.jpg" alt="" /></p>
+    </div>
+}
+
+export default Home;
+ 
